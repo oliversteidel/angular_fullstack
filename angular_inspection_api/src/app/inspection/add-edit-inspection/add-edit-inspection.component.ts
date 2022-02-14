2b7fad531793914f081fbd262d50acfa6eb5088a
Oliver Steidel <oliversteidel@yahoo.de>
2022-02-14 21:14:04 +0100

    create angular project

---
+++ angular_inspection_api/src/app/inspection/add-edit-inspection/add-edit-inspection.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-edit-inspection',
+  templateUrl: './add-edit-inspection.component.html',
+  styleUrls: ['./add-edit-inspection.component.scss']
+})
+export class AddEditInspectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ angular_inspection_api/src/app/inspection/show-inspection/show-inspection.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from "@angular/core";
+import { Observable } from "rxjs";
+import { InspectionApiService } from "src/app/inspection-api.service";
+
+@Component({
+  selector: "app-show-inspection",
+  templateUrl: "./show-inspection.component.html",
+  styleUrls: ["./show-inspection.component.scss"],
+})
+export class ShowInspectionComponent implements OnInit {
+  inspectionList$!: Observable<any[]>;
+  inspectionTypesList$!: Observable<any[]>;
+  inspetionTypesList: any = [];
+
+  // Map to display data associate with foreign keys
+  inspectionTypesMap: Map<number, string> = new Map();
+
+  constructor(private service: InspectionApiService) {}
+
+  ngOnInit(): void {
+    this.inspectionList$ = this.service.getInspectionList();
+  }
+}
